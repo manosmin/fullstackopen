d0825ff1b0f42cfec662c6690b4b821acf8ff78d
manosmin <manosmin@hotmail.com>
2024-12-15 20:36:16 +0200

    part2 exercises

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,45 +1,4 @@
-const Header = ({ title }) => {
-  console.log('Header', title);
-  return (
-    <>
-      <h1>{title}</h1>
-    </>
-  );
-};
-
-const Course = ({ course }) => {
-  console.log('Content', course);
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <>
-    <Header title={course.name}/>
-      {course.parts.map((part) => (
-        <Part key={part.id} name={part.name} exercise={part.exercises}/>
-      ))}
-      <Total total={totalExercises}/>
-    </>
-  );
-};
-
-const Part = ({ name, exercise }) => {
-  console.log('Part', name, exercise);
-  return (
-    <>
-      <p>
-        {name} {exercise}
-      </p>
-    </>
-  );
-};
-
-const Total = ({ total }) => {
-  console.log('Total', total);
-  return (
-    <>
-      <p>Number of exercises {total}</p>
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
