32e30d31bd5895f1898afb470c2b5bf562d93790
manosmin <manosmin@hotmail.com>
2024-12-24 12:54:35 +0200

    part4 exercises

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const Blog = require('../models/blog');
+const blogsRouter = express.Router();
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
+
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body);
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+});
+
+module.exports = blogsRouter;
---
+++ part4/utils/list_helper.js
@@ -0,0 +1,17 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((fav, blog) => (blog["likes"] > (fav["likes"] || 0) ? blog : fav), {})
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+};
