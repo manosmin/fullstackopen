33106de72aaa53b7540386d6c449aaf4e566bb74
manosmin <manosmin@hotmail.com>
2024-12-29 02:21:16 +0200

    part4 exercises

--- part4/index.js
+++ part4/index.js
@@ -1,24 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
+const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const blogsRouter = require('./controllers/blogs');
-
-const app = express();
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('Connected to MongoDB');
-  })
-  .catch((error) => {
-    logger.error('Error connecting to MongoDB:', error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
---
+++ part4/tests/blogs_api.test.js
@@ -0,0 +1,83 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const Blog = require('../models/blog')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are three blogs', async () => {
+    const response = await api.get('/api/blogs')
+  
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('the first blog title is React patterns', async () => {
+    const response = await api.get('/api/blogs')
+  
+    const contents = response.body.map(e => e.title)
+    assert.strictEqual(contents.includes('React patterns'), true)
+})
+
+test('blog without title is not added', async () => {
+    const newBlog = {
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+  })
+
+test('a valid blog can be added ', async () => {
+    const newBlog = {
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(n => n.title)
+    assert(titles.includes('First class tests'))
+})
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    let blogObject = new Blog(helper.initialBlogs[0])
+    await blogObject.save()
+
+    blogObject = new Blog(helper.initialBlogs[1])
+    await blogObject.save()
+
+    blogObject = new Blog(helper.initialBlogs[2])
+    await blogObject.save()
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
