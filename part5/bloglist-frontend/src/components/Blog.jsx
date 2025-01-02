f9b0595178a2625cd171c5b093f18ad7b2f05002
manosmin <manosmin@hotmail.com>
2025-01-02 03:13:53 +0200

    part5 exercises

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,19 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    <h3>{blog.title}</h3>
-    <p>
-      <strong>Author: </strong>
-      {blog.author}
-    </p>
-    <p>
-      <strong>URL: </strong>
-      <a href={blog.url}>{blog.url}</a>
-    </p>
-    <p>
-      <strong>Likes: </strong>
-      {blog.likes}
-    </p>
-  </div>
-);
+import Togglable from "../components/Togglable";
+import { useRef } from "react";
+
+const Blog = ({ blog }) => {
+  const viewBlogRef = useRef();
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <h3>{blog.title}</h3>
+      <Togglable buttonLabel="View blog" ref={viewBlogRef}>
+        <p>
+          <strong>Author: </strong>
+          {blog.author}
+        </p>
+        <p>
+          <strong>URL: </strong>
+          <a href={blog.url}>{blog.url}</a>
+        </p>
+        <p>
+          <strong>Likes: </strong>
+          {blog.likes}
+          <button>Like</button>
+        </p>
+      </Togglable>
+    </div>
+  );
+};
 
 export default Blog;
