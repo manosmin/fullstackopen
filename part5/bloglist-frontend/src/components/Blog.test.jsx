c71215d07e0a1eb028caf196cb40c7c7ae2193e1
manosmin <manosmin@hotmail.com>
2025-01-02 16:41:33 +0200

    part5 exercises

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders title and author but not URL or likes', () => {
+test('renders title and author but not URL and likes by default', () => {
   const blog = {
     title: 'React patterns',
     author: 'Michael Chan',
@@ -25,3 +25,34 @@ test('renders title and author but not URL or likes', () => {
   expect(likes).not.toBeVisible();
 
 })
+
+test('renders URL and likes when button is clicked', () => {
+    const blog = {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 5,
+      user: []
+    }
+  
+    render(<Blog blog={blog} />)
+  
+    const title = screen.getByText('React patterns');
+    expect(title).toBeInTheDocument();
+  
+    const author = screen.getByText('Michael Chan');
+    expect(author).toBeInTheDocument();
+  
+    const url = screen.queryByText('https://reactpatterns.com/');
+    expect(url).not.toBeVisible();
+  
+    const likes = screen.queryByText('5');
+    expect(likes).not.toBeVisible();
+
+    const viewButton = screen.getByText('View blog');
+    fireEvent.click(viewButton);
+
+    expect(url).toBeVisible();
+    expect(likes).toBeVisible();
+  
+  })
