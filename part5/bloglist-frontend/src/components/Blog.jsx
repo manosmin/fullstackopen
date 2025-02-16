dbf43c8661dac42a782ffbd980027a68c419805d
manosmin <manosmin@hotmail.com>
2025-02-16 18:51:37 +0200

    part7 exercises

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -3,12 +3,13 @@ import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setNotification } from "../reducers/notificationReducer";
 import { addLikeToBlog, deleteBlog } from "../reducers/blogReducer";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Blog = ({ blogs }) => {
   const dispatch = useDispatch();
   const userInfo = useSelector((state) => state.user);
   const viewBlogRef = useRef();
+  const navigate = useNavigate();
   const id = useParams().id;
   const blog = blogs.find((b) => b.id === id);
 
@@ -33,6 +34,7 @@ const Blog = ({ blogs }) => {
     if (window.confirm(`Are you sure you want to delete ${name}?`)) {
       try {
         await dispatch(deleteBlog(blogId));
+        navigate("/");
         dispatch(
           setNotification(`Blog "${name}" removed successfully`, "success"),
         );
@@ -45,31 +47,36 @@ const Blog = ({ blogs }) => {
   };
 
   return (
-    <div style={blogStyle} className="blog-item">
-      <h3>{blog.title}</h3>
-      <p>
-        <strong>Author: </strong>
-        {blog.author}
-      </p>
-      <Togglable buttonLabel="View blog" ref={viewBlogRef}>
+    blog && (
+      <div style={blogStyle} className="blog-item">
+        <h3>{blog.title}</h3>
         <p>
-          <strong>URL: </strong>
-          <a href={blog.url}>{blog.url}</a>
+          <strong>Author: </strong>
+          {blog.author}
         </p>
-        <p>
-          <strong>Likes: </strong>
-          {blog.likes}
-          <button className="blog-like-button" onClick={() => handleLike(blog)}>
-            Like
-          </button>
-        </p>
-        {blog.user.some((user) => user.username === userInfo.username) && (
-          <button onClick={() => removeBlog(blog.id, blog.title)}>
-            Remove
-          </button>
-        )}
-      </Togglable>
-    </div>
+        <Togglable buttonLabel="View blog" ref={viewBlogRef}>
+          <p>
+            <strong>URL: </strong>
+            <a href={blog.url}>{blog.url}</a>
+          </p>
+          <p>
+            <strong>Likes: </strong>
+            {blog.likes}
+            <button
+              className="blog-like-button"
+              onClick={() => handleLike(blog)}
+            >
+              Like
+            </button>
+          </p>
+          {blog.user.some((user) => user.username === userInfo.username) && (
+            <button onClick={() => removeBlog(blog.id, blog.title)}>
+              Remove
+            </button>
+          )}
+        </Togglable>
+      </div>
+    )
   );
 };
 
