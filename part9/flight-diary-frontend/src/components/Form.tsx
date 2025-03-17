7997d48e9bfd5881440f0b00b3fd5432ff883a3e
manosmin <manosmin@hotmail.com>
2025-03-17 22:46:06 +0200

    9.19

--- part9/flight-diary-frontend/src/components/Form.tsx
+++ part9/flight-diary-frontend/src/components/Form.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import diaryService from '../services/diaries'
 import { Diary } from "../types";
-
+import axios from "axios";
 interface FormProps {
     diaries: Diary[],
     setDiaries: React.Dispatch<React.SetStateAction<Diary[]>>
@@ -12,10 +12,19 @@ const Form = ({ diaries, setDiaries }: FormProps) => {
     const [visibility, setVisibility] = useState<string>('')
     const [weather, setWeather] = useState<string>('')
     const [comment, setComment] = useState<string>('')
+    const [error, setError] = useState<string>('')
 
     const handleSubmit = (e: React.SyntheticEvent) => {
         e.preventDefault();
-        diaryService.createNew({ date, weather, visibility, comment }).then(response => setDiaries(diaries.concat(response)));
+        diaryService.createNew({ date, weather, visibility, comment }).then(response => setDiaries(diaries.concat(response))).catch((error) => {
+            console.error(error);
+            if (axios.isAxiosError(error) && error.response) {
+                setError(error.response.data);
+                setTimeout(() => {
+                  setError('');
+                }, 5000);
+              }
+        });
         setDate('');
         setVisibility('');
         setWeather('');
@@ -23,25 +32,29 @@ const Form = ({ diaries, setDiaries }: FormProps) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Date </label>
-                <input value={date} onChange={(e) => setDate(e.target.value)}></input>
-            </div>
-            <div>
-                <label>Visibility </label>
-                <input value={visibility} onChange={(e) => setVisibility(e.target.value)}></input>
-            </div>
-            <div>
-                <label>Weather </label>
-                <input value={weather} onChange={(e) => setWeather(e.target.value)}></input>
-            </div>
-            <div>
-                <label>Comment </label>
-                <input value={comment} onChange={(e) => setComment(e.target.value)}></input>
-            </div>
-            <input type="submit" value="Add"></input>
-        </form>
+        <>
+        <div style={{color: 'red'}}>{error}</div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Date </label>
+                    <input value={date} onChange={(e) => setDate(e.target.value)}></input>
+                </div>
+                <div>
+                    <label>Visibility </label>
+                    <input value={visibility} onChange={(e) => setVisibility(e.target.value)}></input>
+                </div>
+                <div>
+                    <label>Weather </label>
+                    <input value={weather} onChange={(e) => setWeather(e.target.value)}></input>
+                </div>
+                <div>
+                    <label>Comment </label>
+                    <input value={comment} onChange={(e) => setComment(e.target.value)}></input>
+                </div>
+                <input type="submit" value="Add"></input>
+            </form>
+        </>
+        
     )
 }
 
